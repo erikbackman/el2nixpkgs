450054d04cbfbd5e092d3b7e8924ffa51f27d891
erikbackman <erikbackman@users.noreply.github.com>
2022-05-22 09:17:01 +0200

    parse args

--- app/Main.hs
+++ app/Main.hs
@@ -3,23 +3,37 @@
 
 module Main where
 
-import Lib
 import Lisp
 import Data.List
 import Data.Maybe
+import System.Directory
+import Control.Monad
+import System.Environment (getArgs)
 
 main :: IO ()
-main = interact $ packagesToNix
-                  . fromMaybe mempty
-                  . traverse extract_setup_el
-                  . parseLisp
+main = do
+  args <- getArgs
+  case args of
+    [] -> putStrLn "Unable to parse lisp"
+    (x:_) -> do
+      e <- doesFileExist x
+      when e $ do
+        src <- readFile x
+        let pkgs = extract_packages src
+        print pkgs
+
+extract_packages :: String -> String
+extract_packages = packagesToNix . fromMaybe mempty . traverse extract_setup_el . parseLisp
 
 -- Nix --------------------------------------------------
 newtype Package = Package { fromPackage :: String }
   deriving (Eq, Show)
 
 extract_setup_el :: LispVal -> Maybe Package
-extract_setup_el (List (Atom "setup":Atom pkg:_)) = Just $ Package pkg
+extract_setup_el (List (Atom "setup":rest)) =
+  case rest of
+    (Atom pkg:_) -> Just $ Package pkg
+    (List (Atom "quote":Atom pkg:_):_) -> Just $ Package pkg
 extract_setup_el _ = Nothing
 
 packagesToNix :: [Package] -> String
