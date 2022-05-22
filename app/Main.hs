f69a6191e59a1b4751f6ad5892e77897df54a597
erikbackman <erikbackman@users.noreply.github.com>
2022-05-22 13:42:48 +0200

    cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,14 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use camelCase" #-}
 {-# LANGUAGE BlockArguments #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
 module Main where
 
-import Lisp
+import Control.Monad
 import Data.List
 import Data.Maybe
+import Lisp
 import System.Directory
-import Control.Monad
 import System.Environment (getArgs)
 import System.IO
 
@@ -16,27 +16,31 @@ main :: IO ()
 main = do
   args <- getArgs
   case args of
-    []               -> putStrLn "Unable to parse lisp"
-    (style:config:_) -> do
+    [] -> putStrLn "Unable to parse lisp"
+    (style : config : _) -> do
       readable <- doesFileExist config
       when readable $ do
         src <- readFile' config
         putStrLn $ maybe "Unsupported" show (extractWith style src)
 
-extractWith :: String -> String -> Maybe String
-extractWith "setup" lisp = extract_packages lisp extract_setup_el
-extractWith _      _     = Nothing
-
-extract_packages :: String -> (LispVal -> Maybe Package) -> Maybe String
-extract_packages src extract = fmap packagesToNix . traverse extract . parseLisp $ src
-
-newtype Package = Package { fromPackage :: String }
+newtype Package = Package {fromPackage :: String}
   deriving (Eq, Show)
 
-extract_setup_el :: LispVal -> Maybe Package
-extract_setup_el (List (Atom "setup":Atom pkg:_)) = Just $ Package pkg
-extract_setup_el _ = Nothing
+extractWith :: String -> String -> Maybe String
+extractWith how = fmap packagesToNix . traverse f . parseLisp
+  where
+    f = case how of
+      "setup" -> extract_setup_el
+      "use-package" -> extract_use_package
+
+extractFor :: String -> LispVal -> Maybe Package
+extractFor k (List (Atom k' : Atom pkg : _)) =
+  if k == k' then Just $ Package pkg else Nothing
+extractFor _ _ = Nothing
+
+extract_setup_el = extractFor "setup"
+
+extract_use_package = extractFor "use-package"
 
 packagesToNix :: [Package] -> String
 packagesToNix pkgs = "[" <> unwords (fmap fromPackage pkgs) <> "]"
-
